791b60d7b1d29288065bd22bf9f5ba7e4190fe22
Philip Wimmer <you@example.com>
2025-12-06 15:53:26 +0100

    Schriften, Groesse und Mobile angepasst

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="{{asset('css/home-style.css')}}">
 </head>
 <body>
 @include('components/menu')
 <main>
+    <i id="LightBulb" class="fa-solid fa-lightbulb fa-2xl" style="color: #ffffff;"></i>
     <h1 id="ger">Montag</h1>
 
     <div class="container">
